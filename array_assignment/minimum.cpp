c3966d71f15a844bf53f31c181f9c5f5938bad19
kathuriakirti <kirtikathuria8@gmail.com>
2023-09-05 20:06:15 +0530

    commit changes

--- array_assignment/minimum.cpp
+++ array_assignment/minimum.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // Find the second largest element in the given Array in one pass.
+
     int arr[6]={3,6,4,9,5,9},min=INT_MAX;
     for(int i=0;i<6;i++){
         if(arr[i]<min) min=arr[i];
